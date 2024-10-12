69af71a2cb67304f1221cf00242397c70f0e7cc5
rafiqi <muhammadrafiqi171@gmail.com>
2024-10-12 22:26:46 +0700

    Create for in.dart

---
+++ for in.dart
@@ -0,0 +1,17 @@
+//INI ADALAH CONTOH TANPA FOR IN
+// void main(){
+//   var array =<String>['Muhammad', 'Rafiqi','.'];
+
+//   for(var i = 0;i< array.length;i++){
+//     print(array[i]);
+//   }
+// }
+
+//INI ADALAH CONTOH MENGGUNAKAN FOR IN
+// void main(){
+//   var array = <String>['Muhammad','Rafiqi','.'];
+
+//   for(var value in array){
+//     print(value);
+//   }
+// }
